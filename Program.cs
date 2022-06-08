e344fc5ca5e13ac78ee674b0b7c683eb4e4ab135
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-08 20:20:37 -0300

    corrige

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
